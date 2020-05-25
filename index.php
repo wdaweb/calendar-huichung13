31bd4eecae90e2a23c5d33df398ab5e0934a2915
moto <moto1019@gmail.com>
2020-05-25 08:13:06 +0800

    calendar with base function

--- index.php
+++ index.php
@@ -1,134 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
 
-   
-<title>月曆製作</title>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calendar</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+    <script src="https://kit.fontawesome.com/2d0f11d24e.js" crossorigin="anonymous"></script>
     <style>
-     body{
-         display: flex;
-         justify-content: center;
-         align-items: center;
-         background: linear-gradient(135deg, #E1DAE7, lightblue);
-         
-     }   
-       
-    
-    .box{
-        width: 600px;
-        height: 300px;
-        border-radius: 10px;
-        background: white;
-        box-shadow: 5px 5px 1rem #333;
-        
-    }
-    table{
-        width= 500px;
-        border-collapse:collapse;
-        margin:auto;
-    }
-    td{
-        border-top:1px solid #999;
-        text-align:center;
-        padding:5px;
-        font-family:Arial, Helvetica, sans-serif;
-        font-size:16px;
-    }
-    
-   
-    tr:nth-of-type(1){
-        background:yellow;
-    }
+        @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;400&display=swap');
+body{
+    font-family: 'Noto Sans TC', sans-serif;
+}
+</style>
 
-   
-    td:nth-of-type(1){
-       
-        color:red;
-    }
-    
-    td:nth-last-child(1){
-        
-        color: green;
-    }
-    </style>
+</head>
 
-<div class="box">
-<?php
+<body>
+    <?php
 
-    $month=date("m");
+    $month = date("m");
+    $year = date("Y");
+
 
     if (isset($_GET["month"])) {
-        $month=$_GET["month"];
+        $month = $_GET["month"];
     }
+    if (isset($_GET["year"])) {
+        $year = $_GET["year"];
+    }
+
+    $timecode = strtotime("Y-m-01");
+    $firstDay = date("$year-$month-1", $timecode);
+    // 當月一號星期幾
+    $firstDayWeek = date("w", strtotime($firstDay));
+    //一個月有幾天
+    $monthdays = date("t", strtotime($firstDay));
+
+    echo "<a href='index.php'>" . $year . "-" . $month . "</a>";
+    if ($month >= 12) {
+        $nextm = 1;
+        $nexty = $year + 1;
+    } else {
+        $nextm = $month + 1;
+        $nexty = $year;
+    }
+    if ($month <= 1) {
+        $prem = 12;
+        $prey = $year - 1;
+    } else {
+        $prem = $month - 1;
+        $prey = $year;
+    }
+
+
+
     ?>
-<table>
-<tr><td colspan='7'>
-<?php 
+    <div class="container table-responsive">
+    <table class="table table-light text-center ">
+<thead class="thead-dark">
+    <tr>
+            <th colspan='7'>
+               
+                
+                <a href="index.php?month=<?= $prem; ?>&year=<?= $prey; ?>"><i class="fas fa-chevron-left"></i></a>
+                <span><?= date("M" , $month); ?>  <?= $year; ?></span>
+                <a href="index.php?month=<?= $nextm; ?>&year=<?= $nexty; ?>"><i class="fas fa-chevron-right"></i></a>
+</th>
+            </tr>
+            <tr>
+                <td>SUN</td>
+                <td>MON</td>
+                <td>TUE</td>
+                <td>WED</td>
+                <td>THU</td>
+                <td>FRI</td>
+                <td>SAT</td>
+            </tr>
 
-echo "TODAY   " . date("d F ") . "<br>";
+            <?php
 
-?>
-<a href="index.php?month=<?=$month-1;?>">上一月</a>
-<span>本月(<?=$month;?>)</span>
-<a href="index.php?month=<?=$month+1;?>">下一月</a>
+            $events = [
+
+                "1-1" => "元旦",
+
+                "2-28" => "和平紀念日",
+                "4-4" => "兒童節",
+
+                "5-1" => "勞動節",
+                "6-25" => "端午節",
+                "10-1" => "中秋節",
+                "10-10" => "雙十節",
+            ];
 
 
-</td></tr>
-<tr>
-        <td>SUN</td>
-        <td>MON</td>
-        <td>TUE</td>
-        <td>WED</td>
-        <td>THU</td>
-        <td>FRI</td>
-        <td>SAT</td>
-    </tr>
-<?php
+            $weeks = ceil(($firstDayWeek + $monthdays) / 7);
+            for ($i = 1; $i <= $weeks; $i++) {
+                echo "<tr>";
+                for ($j = 1; $j < 8; $j++) {
+                    $dayStr = (($i - 1) * 7 + $j) - $firstDayWeek;
 
-   
-if ($month>12) {
-    $month-12;
-}
-  $timecode=strtotime("2020-$month-01");
-  $firstDay=date('Y-m-1',$timecode);
-  $firstDayWeek=date("w",strtotime($firstDay));
-  $days=date("t",strtotime($firstDay));
-  
-  $mdays=[];  
-  for($i=0;$i<$days;$i++){
-    $date=date("Y-m-d",strtotime("+$i days",$timecode)); 
-    $mdays[$date]=date("d",strtotime("+$i days",$timecode)); 
-  }
-  
-  $events=[
-    "2020-1-1"=>"元旦",
-    "2020-1-24"=>"除夕",
-    "2020-1-25"=>"春節",
-    "2020-2-28"=>"228紀念日",
-    "2020-4-4"=>"兒童節",
-    "2020-4-4"=>"掃墓節",  
-    "2020-5-1"=>"勞動節",
-    "2020-6-25"=>"端午節",
-    "2020-10-1"=>"中秋節",
-    "2020-10-10"=>"雙十節",
-           ];
-  
-  for($i=1;$i<=6;$i++){
-      echo "<tr>";
-      for($j=1;$j<=7;$j++){
-  
-          
-          $tmp=($i-1)*7+$j-$firstDayWeek-1;
-          $cellDate=date("Y-m-d",strtotime("+$tmp days",$timecode));
-          echo "<td>";
+                    $tmpDay = date("$year-$month-$dayStr", strtotime("now"));
+                    $eventDate = date("n-j", strtotime($tmpDay));
 
-            
+                    if (!empty($events[$eventDate])) {
+                        $meg = $events[$eventDate];
+                    } else {
+                        $meg = "　";
+                    }
 
-          
-          if(!empty($mdays[$cellDate])){
-            echo $mdays[$cellDate];
-        }
-          echo "</td>";
-      }
-      echo "</tr>";
-  } 
-?>
-</table>
-</div>
+
+                    if ($i == 1) {
+                        if ($dayStr > 0) {
+                            echo "<td> $dayStr <br> $meg </td>";
+                        } else {
+                            echo "<td></td>";
+                        }
+                    } else {
+
+                        if ($dayStr <= $monthdays) {
+                            echo "<td> $dayStr <br> $meg </td>";
+                        } else {
+                            echo "<td></td>";
+                        }
+                    }
+                }
+                echo  "</tr>";
+            }
+
+            ?>
+
+        </table>
+    </div>
+</body>
+
+</html>
